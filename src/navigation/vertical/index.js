0ec5eba6d72c91f2007420bf459a87ed17df65d5
Arif Roska <arif.roska120820@gmail.com>
2023-07-15 19:09:51 +0700

    fix : sidebar menu

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -3,6 +3,12 @@ import Login from 'mdi-material-ui/Login'
 import Table from 'mdi-material-ui/Table'
 import CubeOutline from 'mdi-material-ui/CubeOutline'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
+import InventoryOutline from '@mui/icons-material/Inventory2Outlined';
+import MonetizedOutline from '@mui/icons-material/MonetizationOnOutlined';
+import AssignmentOutline from '@mui/icons-material/AssignmentTurnedInOutlined';
+import FolderSpecialOutline from '@mui/icons-material/FolderSpecialOutlined';
+import EngineeringOutline from '@mui/icons-material/EngineeringOutlined';
+import PeopleOutline from '@mui/icons-material/PeopleOutlineOutlined';
 import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
 import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
 import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
@@ -16,6 +22,36 @@ const navigation = () => {
       title: 'Dashboard',
       icon: HomeOutline,
       path: '/'
+    },
+    {
+      title: 'Inventory',
+      icon: InventoryOutline,
+      path: ''
+    },
+    {
+      title: 'Cash',
+      icon: MonetizedOutline,
+      path: ''
+    },
+    {
+      title: 'Attendance',
+      icon: AssignmentOutline,
+      path: ''
+    },
+    {
+      title: 'Assistant',
+      icon: PeopleOutline,
+      path: ''
+    },
+    {
+      title: 'File',
+      icon: FolderSpecialOutline,
+      path: ''
+    },
+    {
+      title: 'Role',
+      icon: EngineeringOutline,
+      path: ''
     },
     {
       title: 'Account Settings',
